89649ce8a2e8e3d8904549aeca67f166e11d6b42
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-05-28 18:52:57 -0400

    constructed display framework/tailwind css installation

--- campus-quest/src/app/page.js
+++ campus-quest/src/app/page.js
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>
-        Welcome to my App!!
+      
+      <h1 className = "bg-purple-500">
+        Welcome to my App
       </h1>
     </div>
       );
