498dd73ea660b2ff6d47a500c4f307da3341d03f
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-05-29 00:08:44 -0400

    added index page map

--- campus-quest/src/app/page.js
+++ campus-quest/src/app/page.js
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Index() {
   return (
     <div className = "h-screen">
-      
-      <h1 className = "bg-black h-screen">
+      <div>     
+      <Image src="/map.png" alt="Logo" width={40} height={40} className="mr-2 "  />
+      </div>
+
+      <h1 className = "bg-black h-screen text-white">
+        
         Welcome to the start page
       </h1>
     </div>
--- campus-quest/tailwind.config.js
+++ campus-quest/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        "lightlark" : "#111111",
+        "darkbrown": "#492B0A"
+      },
     },
   },
   plugins: [],
