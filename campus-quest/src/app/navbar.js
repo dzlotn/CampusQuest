6cda90e3b927de85d0c21a4206506dc6a80de8fb
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-05-29 22:12:08 -0400

    reconfigured navbar for sign-in

--- campus-quest/src/app/navbar.js
+++ campus-quest/src/app/navbar.js
@@ -5,14 +5,16 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center p-4 bg-lightlark border-b-[0.001rem] border-slate-500">
+    <nav className="flex justify-between items-center p-4 bg-lightlark border-b-[0.001rem] border-slate-700">
       <div className="flex items-center">
-        <Image src="/mapicon.png" alt="Logo" width={40} height={40} className="mr-2 invert"  />
+        <Link href= "/">
+        <Image href = "/" src="/mapicon.png" alt="Logo" width={40} height={40} className="mr-2 invert"  />
+        </Link>
         <span className="text-white text-[1.25rem] font-mono ml-2.5">CampusQuest</span>
       </div>
       <div className="flex space-x-3 mr-3 ">
         <Link href = "/statistics" className="text-white hover:bg-gray-700 hover:underline p-2 rounded">Statistics</Link>
-        <Link href="/contact" className="text-white hover:bg-gray-700 hover:underline p-2 rounded">Contact</Link>
+        <Link href="/contact" className="text-white bg-gray-700 hover:underline p-2 rounded">Sign In</Link>
       </div>
     </nav>
   );
--- campus-quest/src/app/page.js
+++ campus-quest/src/app/page.js
@@ -1,31 +1,46 @@
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Index() {
   return (
-    <body className = "bg-black h">
-    <div className="h-3/4 bg-black flex">
-  <div className="w-1/2 flex flex-col justify-center items-center">
-    <h1 className="text-slate-500 text-9xl font-serif text-center">
-      Track the Journey
-    </h1>
-    <h2 className="text-slate-300 text-4xl font-serif text-center mt-4">
-      Mapping Student Success
-    </h2>
-  </div>
+    <body className="bg-black">
+      <div className="h-3/4 bg-black flex">
+        <div className="w-1/2 flex flex-col justify-center items-center">
+          <h1 className="text-slate-500 text-9xl font-serif text-center">
+            Track the Journey
+          </h1>
+          <h2 className="text-slate-300 text-4xl font-serif text-center mt-4">
+            Mapping Student Success
+          </h2>
+        </div>
 
-  <div className="w-1/2 relative overflow-hidden flex justify-center ">
-    <Image
-      src="https://wallpapercave.com/wp/wp3357174.jpg"
-      alt="sectionimage"
-      className="object-cover"
-      width={1000}
-      height={500}
-    />
-    <div className="absolute inset-0 bg-gradient-to-r from-black from-2% " />
-  </div>
-</div>
-</body>
-);
+        <div className="w-1/2 relative overflow-hidden flex justify-center items-center">
+          <Image
+            src="https://wallpapercave.com/wp/wp3357174.jpg"
+            alt="sectionimage"
+            className="object-cover"
+            width={1000}
+            height={500}
+          />
+          <div className="absolute inset-0 bg-gradient-to-r from-black from-2% " />
+        </div>
+      </div>
+      <div className="bg-black flex h-[50vh] border-t-4 border-dashed border-slate-500">
+        <div className="bg-black flex flex-col w-1/2 justify-center items-center border-r-4 border-radius-2">
+          <h1 className="text-center">a</h1>
+        </div>
+        <div className="bg-green flex w-1/2 justify-center items-center relative overflow-hidden">
+          <Image
+            src="https://wallpapers.com/images/featured/map-pin-png-uif0obj7gubmdh59.jpg"
+            alt="sectionimage"
+            className="object-cover"
+            width={400}
+            height={400}
+          />
+          <div className="absolute inset-0 bg-gradient-to-r from-black from-2%" />
+        </div>
+      </div>
+    </body>
+  );
 }
-
-
