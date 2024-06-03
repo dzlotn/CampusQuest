1efd342c29eb1fecfcc57c036ea17f044bce0e35
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-06-03 13:11:08 -0400

    added college page button

--- campus-quest/src/app/discover/page.js
+++ campus-quest/src/app/discover/page.js
@@ -4,19 +4,23 @@ import Image from 'next/image';
 export default function discover() {
     return (
         <body className="bg-black">
-            <div className=" bg-black image-container relative flex items-center justify-center">
-                <Image
-                    src="/campusphoto.jpg"
-                    alt="sectionimage"
-                    className="brightness-25 rounded-lg"
-                    width={500}
-                    height={500}
-                />
-                <div className="text-overlay absolute left-50 top-30 p-4 text-white font-bold text-2xl">Discover Colleges and Universities</div>
+            <div className="relative bg-black">
+                <div className="image-container relative flex items-center justify-center">
+                    <Image
+                        src="/college.jpg"
+                        alt="sectionimage"
+                        className="rounded-lg"
+                        width={500}
+                        height={500}
+                    />
+                    <h1 className="text-overlay drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] absolute inset-0 flex items-center justify-center text-white font-bold text-2xl ">Discover Colleges and Universities</h1>
+                    <div className="text-box-container absolute inset-x-0 bottom-0 mb-10 flex items-center justify-center">
+                        <input type="text" id="Name" name="Name" placeholder="Search for a school" className="bg-black text-white p-2 border border-white rounded-l-lg"></input>
+                        <button className="bg-green-500 rounded-r-lg px-4 py-2 cursor-pointer active:bg-green-600 border " id= "gobutton">Go</button>
+                    </div>
+                </div>
             </div>
-            <div className = "h-1/3 bg-black"></div>
-            
+            <div className="bg-black">asf</div>
         </body>
-
     );
 }
