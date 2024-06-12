8e522a4fbf0e6772fd60566f3ed6ba1f7a9244e6
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-06-12 09:32:48 -0400

    created getIcon function which uses cheerio to get yahoo logo

--- campus-quest/src/app/discover/search.js
+++ campus-quest/src/app/discover/search.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+const request = require('request');
+const cheerio = require('cheerio');
 
 // Initial text state
 let txt = "";
@@ -49,6 +51,35 @@ async function checkIcon(college) {
         throw new Error("Failed to get college icon for " + college);
     }
 }
+
+function getIcon(college) {
+    const college = "Stanford";
+    const query = college + " logo wikimedia transparent background";
+    const url = `https://images.search.yahoo.com/search/images?p=${encodeURIComponent(query)}`;
+
+    request(url, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const $ = cheerio.load(html);
+            const images = [];
+
+            $('img').each((index, element) => {
+                const imageUrl = $(element).attr('src');
+                if (imageUrl) {
+                    images.push(imageUrl);
+                }
+            });
+
+            if (images.length > 0) {
+                console.log(images[0]);
+            } else {
+                console.error('No images found for the query:', query);
+            }
+        } else {
+            console.error('Failed to fetch images:', error);
+        }
+    });
+}
+
 
 
 export { search, logChanges, checkIcon, executePython };
